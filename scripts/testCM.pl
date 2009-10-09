a467c97b8b97e7a39ad632a0b8c18e4d47fbbaa1
Martin Kamerbeek <martin@oqapi.nl>
2009-10-09 17:58:36 +0200

    Prettyfied the testCM.pl script

--- scripts/testCM.pl
+++ scripts/testCM.pl
@@ -30,15 +30,32 @@ my $time = [ gettimeofday ];
 # Create chart and add datasets to it
 my $chart = Chart::Magick::Chart::Line->new();
 $chart->addData( $dsx, $dsy,   'marker2' );
-$chart->addData( $dsx2, $dsy2, 'gooey.png', 15 ); 
+$chart->addData( $dsx2, $dsy2, 'gooey.png', 20 ); 
 
 # Create coordinate system
 my $axis = Chart::Magick::Axis::Lin->new( {
     width           => 1000,
     height          => 600,
-    xSubtickCount   => 0,
+
+    # Set default font and size
+    font            => '/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf',
+    fontSize        => 12,
+
+    # Override font for title
+    titleFont       => '/usr/share/fonts/truetype/dustin/PenguinAttack.ttf',
+    titleColor      => 'purple',
+    
+    # Set the titles
+    title           => 'Gooey Attack!',
+    xTitle          => 'Prediction',
+    yTitle          => 'Outcome',
+
+    # Normalize the x ticks to pi, and set a custom tick width. 
     xLabelUnits     => pi,
-    xTickWidth      => pi / 4
+    xTickWidth      => pi / 4,
+
+    # Custom format the x labels
+    xLabelFormat    => '%.1fπ',
 } );
 
 # Add the chart to the coordinate system
