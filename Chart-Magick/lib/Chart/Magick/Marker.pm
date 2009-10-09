5e2b8c5a857bb6e675fde376c4c695e6b069ec92
Martin Kamerbeek <martin@oqapi.nl>
2009-10-09 11:37:55 +0200

    New Marker module

--- Chart-Magick/lib/Chart/Magick/Marker.pm
+++ Chart-Magick/lib/Chart/Magick/Marker.pm
@@ -2,12 +2,31 @@ package Chart::Magick::Marker;
 
 use strict;
 
-sub new {
-    my $class   = shift;
-    my $axis    = shift;
-    my $size    = shift;
+use Class::InsideOut;
+use Carp;
 
-    bless { size => $size, axis => $axis };
+readonly axis   => my %axis;
+readonly marker => my %marker;
+readonly size   => my %size;
+
+sub new {
+    my $class       = shift;
+    my $properties  = ref $_[0] eq 'HASH' ? shift : { @_ };
+    
+    my $self = bless {}, $class;
+    register $self;
+
+    my $id = id $self;
+
+    $axis{ $id }    = $properties->{ axis };
+    $size{ $id }    = $properties->{ size }
+    $marker{ $id }  = 
+        exists $properties->{ predefined }  ? $self->getPredefinedMarker( $properties->{ predefined } ) :
+        exists $properties->{ fromFile }    ? $self->getMarkerFromFile( $properties->{ fromFile } )     :
+        exists $properties->{ im }          ? $self->getImageMagickMarker( $properties->{ im } )        :
+        croak "Chart::Magick::Marker->new requires one of the following properties: predefined, fromFile or im";
+
+    return $self;
 }
 
 sub draw {
@@ -16,42 +35,67 @@ sub draw {
     my $y       = shift;
     my $color   = shift || 'lightgray';
 
-    my $size    = $self->{ size };
+    $self->axis->im->Composite(
+        image   => $self->im,
+        gravity => 'Center',
+        x       => $x,
+        y       => $y,
+    }
+}
 
-    my $marker1 = [
-        [ 'M', 0,    0      ],
-        [ 'L', 0.5, -0.75   ],
-        [ 'L', 1,    0      ],
-        [ 'Z'               ],
-    ];
+sub getPredefinedMarker {
+    my $self    = shift;
+    my $shape   = shift || 'marker2';
 
-    my $marker2 = [
-        [ 'M',  0,   0      ],
-        [ 'L',  1,   0      ],
-        [ 'L',  1,   1      ],
-        [ 'L',  0,   1      ],
-        [ 'Z'               ],
-    ];
+    my $size    = $size{ id $self };
 
-    my $path = join(' ', map { 
-        $_->[0] .' '
-        . ( defined $_->[1] ? $size*$_->[1].','. $size*$_->[2] : '' )
-    } @$marker2);
+    my $markers = {
+        marker1 => {
+            width   => 1,
+            height  => 0.75,
+            shape   => [
+                [ 'M', 0,    0.75   ],
+                [ 'L', 0.5,  0      ],
+                [ 'L', 1,    0.75   ],
+                [ 'L', 0,    0.75   ],
+            ],
+        },
+        marker2 => [
+            width   => 1,
+            height  => 1,
+            shape   => [
+                [ 'M',  0,   0      ],
+                [ 'L',  1,   0      ],
+                [ 'L',  1,   1      ],
+                [ 'L',  0,   1      ],
+                [ 'L',  0,   0      ],
+            ],
+        },
+    };
 
-    my $translateX = int( $x - 0.5*$size + 0.5 );
-    my $translateY = int( $y - 0.5*$size + 0.5 );
+    my $marker  = $markers->{ $shape };
+    my $path    = join ' ', map { $_->[0] . $size*$_[1] . ',' . $size*$_[2] } @{ $marker->{ shape };
+    my $width   = $size * $marker->{ width  };
+    my $height  = $size * $marker->{ height };
 
-    $self->{ axis }->im->Draw(
+#    my $translateX = int( $x - 0.5*$size + 0.5 );
+#    my $translateY = int( $y - 0.5*$size + 0.5 );
+
+    my $im = Image::Magick->new( width => $width, height => $height );
+    $im->ReadImage( 'xc:none' );
+    $im->Draw(
        primitive    => 'Path',
-       stroke       => $color,
+       stroke       => 'lightgrey', #$color,
        strokewidth  => 1,
        points       => $path,
        fill         => 'none',
        # Use an affine transform here, since the translate option doesn't work at all...
 #       translate    => [ 100, 100 ], #"$translateX,$translateY",
-       affine       => [ 1, 0, 0, 1, $translateX,$translateY ],
+#       affine       => [ 1, 0, 0, 1, $translateX,$translateY ],
        antialias    => 'true',
     );
+
+    return $im;
 }    
 
 1;
