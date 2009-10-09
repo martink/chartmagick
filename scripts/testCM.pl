f0a3c4577e829190cde887aaae8fb0dbacbf500c
Martin Kamerbeek <martin@oqapi.nl>
2009-10-09 17:47:08 +0200

    Adding gooey image for use as test marker and updated the testCM script to use it.

--- scripts/testCM.pl
+++ scripts/testCM.pl
@@ -14,20 +14,25 @@ use Time::HiRes qw( gettimeofday tv_interval );
 use constant pi => 3.14159265358979;
 
 my $pxCount = 1000;
+
+
+# Generate dataset 1
 my $dsx = [ map { pi / $pxCount * $_          } (-$pxCount/2 .. $pxCount/2) ];
-my $dsy = [ map { sin( 50*$_ ) + sin( 61*$_ )   } @{ $dsx } ];
+my $dsy = [ map { sin( 50*$_ ) + sin( 61*$_ ) } @{ $dsx } ];
 
+# Generate dataset 2
+my $dsx2 = [ map { pi / $pxCount * 16 * $_      } ( -$pxCount / 32 .. $pxCount / 32 ) ];
+my $dsy2 = [ map { $_ * pi + sin( 10*$_ )       }  @{ $dsx2 } ];
 
-my $chart2  = Chart::Magick::Chart::Line->new();
-$chart2->dataset->addDataset( $dsx, $dsy );
-
-
-$dsx = [ map { pi / $pxCount * 16 * $_      } ( -$pxCount / 32 .. $pxCount / 32 ) ];
-$dsy = [ map { $_ * pi + sin( 10*$_ )              }  @{ $dsx } ];
-    
-$chart2->dataset->addDataset( $dsx, $dsy ); 
-
+# Timekeeping
 my $time = [ gettimeofday ];
+
+# Create chart and add datasets to it
+my $chart = Chart::Magick::Chart::Line->new();
+$chart->addData( $dsx, $dsy,   'marker2' );
+$chart->addData( $dsx2, $dsy2, 'gooey.png', 15 ); 
+
+# Create coordinate system
 my $axis = Chart::Magick::Axis::Lin->new( {
     width           => 1000,
     height          => 600,
@@ -35,13 +40,19 @@ my $axis = Chart::Magick::Axis::Lin->new( {
     xLabelUnits     => pi,
     xTickWidth      => pi / 4
 } );
-$axis->addChart( $chart2 );
+
+# Add the chart to the coordinate system
+$axis->addChart( $chart );
+
+# Render it
 $axis->draw;
 
+# And write it to disk
 $axis->im->Write('out.png');
 
+# More timekeeping
 my $runtime = tv_interval( $time );
 
 print "___>$runtime<___\n";
-print $chart2->dataset->memUsage;
+print $chart->dataset->memUsage;
 #print join "\n" , $canvas->im->QueryFont;
