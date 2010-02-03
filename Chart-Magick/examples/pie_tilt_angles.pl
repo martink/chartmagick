6b5f4b3227446ab64c8af4bd04c1d40670737af3
Martin Kamerbeek <martin@oqapi.nl>
2010-02-03 15:47:37 +0100

    Added some comments

--- Chart-Magick/examples/pie_tilt_angles.pl
+++ Chart-Magick/examples/pie_tilt_angles.pl
@@ -1,14 +1,15 @@
 use strict;
 use warnings;
 
-use Chart::Magick::Palette;
 use Chart::Magick;
 
+# Simple dataset 
 my $data = [
-    [  1,  2, 3, 4, 5   ],
-    [ 10, 25, 4, 12, 30 ],
+    [  1,  2,  3,  4,  5 ],
+    [ 10, 25,  5, 12, 30 ],
 ];
 
+# Create 6 different pies with different tilt angles
 my @pies;
 for ( 0 .. 5 ) {
     my $tilt = 15 * $_;
@@ -24,10 +25,12 @@ for ( 0 .. 5 ) {
     );
 }
 
+# Put the pies in a matrix.
 my $matrix = Chart::Magick->matrix( 1000, 600, [
     [ @pies[ 0 .. 2 ] ],
     [ @pies[ 3 .. 5 ] ],
 ]);
 
+# And write the image to disk.
 $matrix->write( 'pie_tilt_angles.png' );
 
