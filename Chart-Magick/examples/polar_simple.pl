3029f520bbaf39355d8cad765b6f8d1d3c64b84f
Martin Kamerbeek <martin@oqapi.nl>
2010-02-03 17:24:24 +0100

    Adding polar example.

---
+++ Chart-Magick/examples/polar_simple.pl
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+
+use Chart::Magick;
+
+use Data::Dump qw( ddx );
+
+
+my $data = [
+    [ 0 .. 300 ],
+    [ 0 .. 300 ],
+];
+
+my @charts;
+for ( 1 .. 4 ) {
+    my $x_stop = 300 / $_;
+    push @charts, Chart::Magick->line(
+        width       => 300,
+        height      => 300,
+        axisType    => 'Polar',
+        data        => [ $data ],
+        axis        => {
+            xStop   => $x_stop,
+            title   => "xStop = $x_stop",
+        },
+    );
+}
+
+my $matrix = Chart::Magick->matrix( 600, 600, [ [ @charts[ 0 .. 1 ] ], [ @charts[ 2 .. 3 ] ] ]);
+$matrix->write( 'polar_simple.png' );
+
+ddx( $matrix->getAxis( 0,0 )->plotOption )
+
